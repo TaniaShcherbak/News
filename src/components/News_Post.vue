f0f1f5a1e42b41ce7ff2f2a5d67a3656cb0a513c
DariaZhur <dariazhur89@gmail.com>
2023-09-20 22:23:12 +0300

    add card style

--- src/components/News_Post.vue
+++ src/components/News_Post.vue
@@ -3,8 +3,8 @@
         <a :href='postinfo.link' target="_blank">
             <h2>{{ postinfo.title }}</h2>
             <p>{{ postinfo.author }}</p>
-            <p>{{ postinfo.desc }}</p>
             <img :src='postinfo.image' alt="image">
+            <p>{{ postinfo.desc }}</p>
         </a>
     </div>
 </template>
@@ -25,13 +25,38 @@ export default {
     padding: 20px;
     text-align: center;
     cursor: pointer;
+    background-color: rgb(240, 235, 235);
+    border-radius: 3%;
+    margin: 20px;
+    box-shadow: 5px 5px 10px #f5f3f3;
+    transition: background-color 0.3s, box-shadow 0.3s;
+    font-size: 28px;
+    transition: transform 0.3s;
+}
+
+.post:hover {
+    transform: scale(1.05);
 }
 
 img {
-    width: 200px;
+    width: 80%;
 }
+
 a {
     text-decoration: none;
     color: black;
+}
+
+.post:hover {
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
+}
+
+.post h2 {
+    transition: color 0.3s;
+    font-size: 32px;
+}
+
+.post:hover h2 {
+    color: brown;
 }
 </style>
