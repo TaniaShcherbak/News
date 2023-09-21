a2339a54f67c8f54ea421484a3d9c357c8f758cb
Oleg Shcherbak <boec603@gmail.com>
2023-09-21 23:04:40 +0300

    update the style of search input

--- src/components/search.vue
+++ src/components/search.vue
@@ -1,35 +1,54 @@
 <template>
     <div class="search">
-        <input type="text" placeholder="search" name="search" id="searchInput" @keyup.enter="getrequest">
+        <input class="input"
+            type="text"
+            placeholder="search"
+            name="search"
+            id="searchInput"
+            @keyup.enter="getrequest"
+        />
+        <img  @click="getrequest" class="searchImage" src="../../public/search.png" alt="search">
     </div>
 </template>
 <script>
 export default {
     name: "searchBar",
-    methods:
-    {
+    methods: {
         getrequest() {
             let search = document.getElementById("searchInput").value;
-            this.$emit('searchQuerry', search);
-            document.getElementById("searchInput").value='';
-        }
-    }
-}
+            this.$emit("searchQuerry", search);
+            document.getElementById("searchInput").value = "";
+        },
+    },
+};
 </script>
 
 <style>
-div input{
-    padding: 0;
-    width: 200px;
-    height: 30px;
-    margin-bottom: 30px;
+.search{
+    display: flex;
+    flex-direction: row;
+    background-color: rgb(250, 248, 248);
+    align-self: center;
+    height: 50px;
     border-radius:25px;
-    border:none;
-    background-color: rgb(245, 244, 244);
-    margin-top: 30px;
+    width: 250px;
+
 }
-input::placeholder{
+.searchImage{
+    width: 50px;
+    height: 50px;
+    cursor: pointer;
+}
+div input {
+    padding: 0;
+    border-radius: 25px;
+    border: none;
+    opacity: 80%;
+    width: 250px;
+    margin-left: 10px;
+    outline: none;
+}
+input::placeholder {
     padding-left: 10px;
 }
-
 </style>
