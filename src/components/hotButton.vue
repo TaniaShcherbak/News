54b3cf9a8fcaa643bf05df8c926e29fd2c9d5167
TaniaShcherbak <fabrika7km@gmail.com>
2023-09-22 15:18:37 +0300

    Added hot panel

---
+++ src/components/hotButton.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <button name="title" @click="hotQuerry">
+            {{ hotButton.title }}
+        </button>
+    </div>
+</template>
+<script>
+export default {
+    name: 'hotButton',
+    props: {
+        hotButton: {},
+    },
+    methods: {
+        hotQuerry() {
+            this.$emit("hotSearch",this.hotButton.title);
+        }
+    }
+}
+</script>
