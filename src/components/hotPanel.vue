0ca7295ea11f27973d75618052fa7eae8462b49a
TaniaShcherbak <fabrika7km@gmail.com>
2023-09-26 11:00:03 +0300

    Added scroll to top

--- src/components/hotPanel.vue
+++ src/components/hotPanel.vue
@@ -55,9 +55,6 @@ export default {
     components: {
         hotButton,
     },
-    // watch: {
-    //     hotSearch: 'onHotSearch'
-    // },
     methods: {
         onHotSearch(data) {
             this.$emit("hotSearch", data);
